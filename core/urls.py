e0f412436f263eb1ea624720b511d06825c901f6
JawadArman96 <jawadarman738@gmail.com>
2023-03-26 03:23:22 +0900

    Add add-to-cart function and view orderItem in admin panel

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from .views import ItemDetailView, check_out, HomeView
+from .views import (
+    ItemDetailView,
+    check_out,
+    HomeView,
+    add_to_cart
+)
 
 app_name = 'core'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='item_list'),
     path('checkout/', check_out, name='check_out'),
-    path('products/<slug>', ItemDetailView.as_view(), name='products')
+    path('products/<slug>', ItemDetailView.as_view(), name='products'),
+    path('add-to-cart/<slug>', add_to_cart, name='add-to-cart')
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from .models import Item
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Item, OrderItem, Order
 from django.views.generic import ListView, DetailView
+from django.utils import timezone
 
 
 class HomeView(ListView):
@@ -16,3 +17,18 @@ class ItemDetailView(DetailView):
 def check_out(request):
     return render(request, "checkout-page.html")
 
+
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_item = OrderItem.objects.create(item=item)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+        else:
+            ordered_date = timezone.now()
+            order = Order.objects.create(user=request.user, ordered_date=ordered_date)
+            order.items.add(order_item)
+    return redirect("core:products", slug=slug)
