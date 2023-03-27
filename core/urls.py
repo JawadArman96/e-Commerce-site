e00268712c753c6a8a325e3db32ca356e8af67dc
JawadArman96 <jawadarman738@gmail.com>
2023-03-27 16:53:57 +0900

    Add removing item from cart module

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,8 @@ from .views import (
     ItemDetailView,
     check_out,
     HomeView,
-    add_to_cart
+    add_to_cart,
+    remove_from_cart
 )
 
 app_name = 'core'
@@ -12,5 +13,6 @@ urlpatterns = [
     path('', HomeView.as_view(), name='item_list'),
     path('checkout/', check_out, name='check_out'),
     path('products/<slug>', ItemDetailView.as_view(), name='products'),
-    path('add-to-cart/<slug>', add_to_cart, name='add-to-cart')
+    path('add-to-cart/<slug>', add_to_cart, name='add-to-cart'),
+    path('remove_from_cart/<slug>', remove_from_cart, name='remove-from-cart')
 ]
